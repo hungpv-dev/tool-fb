5d98fcdedb209c6dbbca9ed71d6763e7432a87ec
hungpv <phamvanhung.dev@gmail.com>
2025-01-03 09:12:57 +0700

    done

--- base/browser.py
+++ base/browser.py
@@ -31,12 +31,22 @@ class Browser:
             chrome_options.add_argument("--headless")  # Chế độ không giao diện
             chrome_options.add_argument("--no-sandbox")  # Không sử dụng sandbox
             chrome_options.add_argument("--disable-dev-shm-usage")  # Giải quyết lỗi bộ nhớ (dành cho container)
+            # Tăng hiệu xuất khi chạy headless
+            chrome_options.add_argument("--disable-gpu")  # Tắt GPU khi headless
 
-
+        # Tối ưu tình huống cụ thể
         chrome_options.add_argument("--disable-notifications")  # Tắt thông báo
         chrome_options.add_argument("--disable-popup-blocking")  # Tắt chặn popup
         chrome_options.add_argument("--disable-extensions")  # Tắt các tiện ích mở rộng không cần thiết
+        chrome_options.add_argument("--disable-translate")  # Tắt dịch trang
+        chrome_options.add_argument("--disable-infobars")  # Tắt thanh thông tin của Chrome
+        chrome_options.add_argument("--disable-browser-side-navigation")  # Tắt tối ưu điều hướng
 
+        # #  Tăng tốc phần cứng!
+        chrome_options.add_argument("--enable-unsafe-swiftshader")  # Cho phép SwiftShader nếu không có GPU
+
+        # Tăng cường bảo mật và tránh bị phát hiện sử dụng Selenium
+        chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 
         service = Service('chromedriver.exe') 
         browser = webdriver.Chrome(service=service,options=chrome_options)
