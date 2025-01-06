379d2c7f783d74d95ea7a3ac93b2d3945f516928
hungpv <phamvanhung.dev@gmail.com>
2025-01-06 22:52:03 +0700

    no cache

--- base/browser.py
+++ base/browser.py
@@ -48,11 +48,13 @@ class Browser:
         if headless:
             chrome_options.add_argument("--headless")
             chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--disable-gpu")
 
         chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
         chrome_options.add_argument("--disable-notifications")
         chrome_options.add_argument('--ignore-certificate-errors')
         chrome_options.add_argument('--ignore-ssl-errors')
+        chrome_options.add_argument('--disable-application-cache')
         chrome_options.add_argument("--disable-translate")
         chrome_options.add_argument("--disable-blink-features=AutomationControlled")
         chrome_options.add_argument("--disable-infobars")
